4d0c1a835d1291a61a15928c4e0789dbb9baa89c
xing_seng <xing_seng@hunliji.com>
2020-07-06 23:58:48 +0800

    不同路径及一致性hash

---
+++ src/com/leetcode/LeetCode_63.java
@@ -0,0 +1,33 @@
+package com.leetcode;
+
+/**
+ * 63. 不同路径 II(medium)
+ * https://leetcode-cn.com/problems/unique-paths-ii
+ * 动态规划
+ * 状态转移方程 f(i, j) = 0,u(i, j) = 0或f(i, j) = f(i - 1, j) + f(i, j - 1), u(i, j) ≠ 0
+ * 滚动数组思想
+ * 动规分类：一、求最优解类（递推性质叫最优子结构），典型问题是背包问题；二、计数类，统计方案数问题（此题）
+ *
+ * @author xing_seng
+ * @date 2020/7/6
+ */
+public class LeetCode_63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] f = new int[m];
+
+        f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+        return f[m - 1];
+    }
+}
---
+++ src/jiagou/ConsistentHashWithVirtualNode.java
@@ -0,0 +1,141 @@
+package jiagou;
+
+import java.util.*;
+
+/**
+ * 带虚拟节点的一致性hash
+ *
+ * @author xing_seng
+ * @date 2020/7/5
+ */
+public class ConsistentHashWithVirtualNode {
+
+    private String[] serversArr;
+
+    // 真实服务器节点
+    private List<String> realNodes = new LinkedList<>();
+
+    // 虚拟节点，key表示虚拟节点的hash值，value表示虚拟节点名称
+    private SortedMap<Integer, String> virtualNodes = new TreeMap<>();
+
+    // 服务器访问统计
+    private TreeMap<String, Integer> serverVisit = new TreeMap<>();
+
+    // 虚拟节点数量
+    private int virtualNodesNum;
+
+    public ConsistentHashWithVirtualNode(String[] servers, int virtualNodesNum) {
+        this.serversArr = servers;
+        this.virtualNodesNum = virtualNodesNum;
+    }
+
+    /**
+     * 把原始服务器添加到真实节点列表中
+     */
+    public void addServer() {
+        realNodes.addAll(Arrays.asList(serversArr));
+    }
+
+    /**
+     * 添加虚拟节点
+     */
+    public void addVirtualNode() {
+        for (String node : realNodes) {
+            if (!serverVisit.containsKey(node)) {
+                serverVisit.put(node, 0);
+                for (int i = 0; i < this.virtualNodesNum; i++) {
+                    int hash = getHash(node + "&VN" + i);
+                    virtualNodes.put(hash, node);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取访问统计
+     */
+    public void getServerVisit() {
+        for (Map.Entry<String, Integer> entry : serverVisit.entrySet()) {
+            System.out.println("服务器节点：" + entry.getKey() + ",命中数量:" + entry.getValue());
+        }
+    }
+
+    /**
+     * FNV1_32_HASH算法
+     *
+     * @param str 计算字符
+     * @return int
+     */
+    private static int getHash(String str){
+        final int p = 16777619;
+        int hash = (int)2166136261L;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash ^ str.charAt(i)) * p;
+        }
+        hash += hash << 13;
+        hash ^= hash >> 7;
+        hash += hash << 3;
+        hash ^= hash >> 17;
+        hash += hash << 5;
+
+        return hash;
+    }
+
+    public void getServer(String key) {
+        // 获取key的hash值
+        int hash = getHash(key);
+        // 获取大于改hash值的所有Map
+        SortedMap<Integer, String> subMap = virtualNodes.tailMap(hash);
+        // 虚拟节点
+        String virtualNode;
+        if (subMap.isEmpty()) {
+            // 为空，获取第一个node
+            virtualNode = virtualNodes.get(virtualNodes.firstKey());
+        } else {
+            // 不为空，找顺时针第一个节点
+            virtualNode = virtualNodes.get(subMap.firstKey());
+        }
+        // 记录访问次数
+        serverVisit.put(virtualNode, serverVisit.get(virtualNode) + 1);
+    }
+
+    // 求标准差
+    public double getStd() {
+        Integer[] visitData = new Integer[serverVisit.size()];
+        serverVisit.values().toArray(visitData);
+        double avg = Arrays.stream(visitData).mapToInt(Integer::intValue).average().orElse(0d);
+        double avgStd = Arrays.stream(visitData).map(count -> Math.pow(count - avg, 2)).mapToDouble(Double::doubleValue).average().orElse(0d);
+        return Math.sqrt(avgStd);
+    }
+
+    public static void main(String[] args) {
+        // 模拟100万数据
+        int count = 1000000;
+        // 10个服务器节点
+        String[] servers = {
+                "192.168.0.0:8080",
+                "192.168.0.1:8080",
+                "192.168.0.2:8080",
+                "192.168.0.3:8080",
+                "192.168.0.4:8080",
+                "192.168.0.5:8080",
+                "192.168.0.6:8080",
+                "192.168.0.7:8080",
+                "192.168.0.8:8080",
+                "192.168.0.9:8080"
+        };
+        // 虚拟节点数量100、200、500
+        int[] virtualNums = new int[]{100, 200, 500};
+        for (int virtualNum : virtualNums) {
+            ConsistentHashWithVirtualNode consistentHashWithVirtualNode = new ConsistentHashWithVirtualNode(servers, virtualNum);
+            consistentHashWithVirtualNode.addServer();
+            consistentHashWithVirtualNode.addVirtualNode();
+            for (int i = 0; i < count; i++) {
+                consistentHashWithVirtualNode.getServer(UUID.randomUUID().toString());
+            }
+            consistentHashWithVirtualNode.getServerVisit();
+            System.out.println("虚拟节点数量：" + virtualNum + ",标准差：" + consistentHashWithVirtualNode.getStd());
+            System.out.println("-------------------------------------------");
+        }
+    }
+}
