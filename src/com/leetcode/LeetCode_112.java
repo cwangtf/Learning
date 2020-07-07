7c43c1fdab8761010eef093145575bce61da2c94
xing_seng <xing_seng@hunliji.com>
2020-07-07 23:04:28 +0800

    112. 路径总和

---
+++ src/com/leetcode/LeetCode_112.java
@@ -0,0 +1,57 @@
+package com.leetcode;
+
+import com.leetcode.utils.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 112. 路径总和(easy)
+ * https://leetcode-cn.com/problems/path-sum
+ * 广度优先搜索、递归
+ *
+ * @author xing_seng
+ * @date 2020/7/7
+ */
+public class LeetCode_112 {
+    /** BFS */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<>();
+        Queue<Integer> queVal = new LinkedList<>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode now = queNode.poll();
+            int temp = queVal.poll();
+            if (now.left == null && now.right == null) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (now.left != null) {
+                queNode.offer(now.left);
+                queVal.offer(now.left.val + temp);
+            }
+            if (now.right != null) {
+                queNode.offer(now.right);
+                queVal.offer(now.right.val + temp);
+            }
+        }
+        return false;
+    }
+
+    /** 递归 */
+    public boolean hasPathSum1(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum1(root.left, sum - root.val) || hasPathSum1(root.right, sum - root.val);
+    }
+}
---
+++ src/com/leetcode/LeetCode_112_1.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+import com.leetcode.utils.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author xing_seng
+ * @date 2020/7/7
+ */
+public class LeetCode_112_1 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<>();
+        Queue<Integer> queVal = new LinkedList<>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode node = queNode.poll();
+            int temp = queVal.poll();
+            if (node.left == null && node.right == null) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (node.left != null) {
+                queNode.offer(node.left);
+                queVal.offer(node.left.val + temp);
+            }
+            if (node.right != null) {
+                queNode.offer(node.right);
+                queVal.offer(node.right.val + temp);
+            }
+        }
+        return false;
+    }
+
+    public boolean hasPathSum1(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum1(root.left, sum - root.val) || hasPathSum1(root.right, sum - root.val);
+    }
+}
