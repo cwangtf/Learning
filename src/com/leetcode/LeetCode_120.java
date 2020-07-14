2c0d3f8eb1ebf960e401f6b01c559f62204b00d5
xing_seng <xing_seng@hunliji.com>
2020-07-14 23:39:52 +0800

    120. 三角形最小路径和

---
+++ src/com/leetcode/LeetCode_120.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+import java.util.List;
+
+/**
+ * 120. 三角形最小路径和(medium)(经典题)
+ * https://leetcode-cn.com/problems/triangle
+ * 一、动态规划；二、动态规划+空间优化
+ *
+ * @author xing_seng
+ * @date 2020/7/14
+ */
+public class LeetCode_120 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] f = new int[n][n];
+        f[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            f[i][0] = f[i - 1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; ++j) {
+                f[i][j] = Math.min(f[i - 1][j - 1], f[i - 1][j]) + triangle.get(i).get(j);
+            }
+            f[i][i] = f[i - 1][i - 1] + triangle.get(i).get(i);
+        }
+        int minTotal = f[n - 1][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[n - 1][i]);
+        }
+        return minTotal;
+    }
+}
