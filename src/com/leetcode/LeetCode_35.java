47bda2e978752e210767e3518d23a544108023d2
xing_seng <xing_seng@hunliji.com>
2020-07-17 23:29:56 +0800

    35. 搜索插入位置

---
+++ src/com/leetcode/LeetCode_35.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+/**
+ * 35. 搜索插入位置(easy)
+ * https://leetcode-cn.com/problems/search-insert-position
+ * 二分查找
+ * 时间复杂度O(logN),空间复杂度O(1)
+ *
+ * @author xing_seng
+ * @date 2020/7/17
+ */
+public class LeetCode_35 {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
