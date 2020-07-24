9e973d1211723f3ce5f099278490a0aecc22c2f3
xing_seng <xing_seng@hunliji.com>
2020-07-24 21:05:30 +0800

    1025. 除数博弈

---
+++ src/com/leetcode/LeetCode_1025.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+/**
+ * 1025. 除数博弈(easy)
+ * https://leetcode-cn.com/problems/divisor-game
+ * 智力题
+ *
+ * @author xing_seng
+ * @date 2020/7/24
+ */
+public class LeetCode_1025 {
+    public boolean divisorGame(int N) {
+        // 最佳状态
+        return N % 2 == 0;
+    }
+}
