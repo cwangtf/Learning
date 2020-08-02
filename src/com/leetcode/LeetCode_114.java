5c8d327bd452aff5199433dbdea17ca6e742c59e
xing_seng <xing_seng@hunliji.com>
2020-08-02 11:12:44 +0800

    114. 二叉树展开为链表

---
+++ src/com/leetcode/LeetCode_114.java
@@ -0,0 +1,61 @@
+package com.leetcode;
+
+import com.leetcode.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 114. 二叉树展开为链表(medium)
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ * 一、前序遍历，递归/迭代，时间复杂度O(n)，空间复杂度O(n)
+ * 二、前序遍历+展开同步
+ * 三、寻找前驱节点，时间复杂度O(n)，空间复杂度O(1)
+ *
+ * @author xing_seng
+ * @date 2020/8/2
+ */
+public class LeetCode_114 {
+    /** 递归 */
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+
+    public void flatten1(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                list.add(node);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+}
---
+++ src/com/leetcode/LeetCode_632.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * 632. 最小区间(hard)
+ * https://leetcode-cn.com/problems/smallest-range-covering-elements-from-k-lists
+ *
+ *
+ * @author xing_seng
+ * @date 2020/8/2
+ */
+public class LeetCode_632 {
+}
