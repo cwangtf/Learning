fdb95c342b06985a2cdcf288d85956a63e2e5595
xing_seng <xing_seng@hunliji.com>
2020-08-02 21:59:49 +0800

    助教参考答案

--- src/jiagou/PressureTest.java
+++ src/jiagou/PressureTest.java
@@ -1,36 +1,42 @@
 package jiagou;
 
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
- * 压测代码参考
- *
- * @author xing_seng
- * @date 2020/7/24
- */
+ * @program: consistencyhash1
+ * @description: 压测
+ * @author: zhanglin16
+ * @create: 2020-07-13 18:27
+ **/
 public class PressureTest {
     private static final String USER_AGENT = "HttpClient/3.1/PP";
     private static final String CHARSET_UTF = "UTF-8";
-    /** 多线程中安全的使用httpClient */
     private MultiThreadedHttpConnectionManager httpConnectionManager = new MultiThreadedHttpConnectionManager();
     private HttpClient httpClient = new HttpClient(httpConnectionManager);
     private ExecutorService executor;
-    /** 测试url */
+    /**
+     * 测试url
+     */
     private String testUrl;
-    /** 请求总次数 */
+    /**
+     * 请求总次数
+     */
     private Integer testLength;
-    /** 并发数 */
+    /**
+     * 并发数
+     */
     private Integer currentCount;
 
     private SortedMap<Long, Integer> testResult = new TreeMap<>();
@@ -42,16 +48,21 @@ public class PressureTest {
         this.executor = Executors.newFixedThreadPool(currentCount);
     }
 
+    /**
+     * 发起测试
+     */
     public void testing(Integer percentTime) {
         for (int i = 0; i < testLength; i++) {
             executor.execute(() -> {
                 long startTime = System.currentTimeMillis();
+                Integer result = null;
                 try {
-                    httpGet(testUrl);
+                    result = httpGet(testUrl);
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
-                long time = System.currentTimeMillis() - startTime;
+                long endTime = System.currentTimeMillis();
+                long time = endTime - startTime;
                 synchronized (testResult.getClass()) {
                     Integer getVal = testResult.get(time);
                     if (getVal == null) {
@@ -67,48 +78,51 @@ public class PressureTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        // 计算响应时长
+        //计算响应时长
         testResultTime(percentTime);
     }
-
+    /**
+     * 响应时常
+     */
     public void testResultTime(Integer percentTime) {
-        // 总时长
-        long timeCount = 0L;
+        //总时长
+        Long timeCount = 0L;
+        // 95时间点
+        Long time95 = 0L;
         // 95总时长
-        long timeCunt95 = 0L;
+        Long timeCount95 = 0L;
         // 统计95的数量
-        double count95 = 0.0;
-        // 用百分比转化为需要查看的请求数的平均响应时长
-        double countTime = testLength * percentTime * 0.01;
-        for(Map.Entry<Long, Integer> entry : testResult.entrySet()) {
+        Double count95 = 0.0;
+        //用百分比转化为需要查看的请求数的平均响应时常
+        Double countTime = testLength * percentTime * 0.01;
+        for (HashMap.Entry<Long, Integer> entry : testResult.entrySet()) {
             timeCount += entry.getKey() * entry.getValue();
-            if (count95 < countTime) {
-                if ((count95 = count95 + entry.getValue() * 1.0) >= countTime) {
-                    timeCunt95 = timeCount;
+            if(count95 < countTime){
+                if((count95 = count95 + entry.getValue() * 1.0) >= countTime){
+                    timeCount95 = timeCount;
+                    time95 = entry.getKey();
                 }
             }
+
         }
         System.out.println("平均响应时间：" + (timeCount / testLength));
-        System.out.println("95平均响应时间：" + (timeCunt95 / countTime));
+        System.out.println("95响应时间：" + time95);
+        System.out.println("95平均响应时间：" + (timeCount95/countTime));
     }
 
     public Integer httpGet(String fullUrl) {
         GetMethod method = new GetMethod(fullUrl);
-        method.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, CHARSET_UTF);
+        method.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "utf-8");
         method.addRequestHeader("User-Agent", USER_AGENT);
         try {
             return httpClient.executeMethod(method);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            // 释放连接
+        }finally {
             method.releaseConnection();
         }
         return 0;
+
     }
 
-    public static void main(String[] args) {
-        PressureTest pressureTest = new PressureTest("https://www.baidu.com/", 100, 10);
-        pressureTest.testing(95);
-    }
 }
