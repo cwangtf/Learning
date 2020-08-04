f86dbafe40580f233054b5069095d7969fe24428
xing_seng <xing_seng@hunliji.com>
2020-08-04 22:51:49 +0800

    207. 课程表

---
+++ src/com/leetcode/LeetCode_207_dfs.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 207. 课程表(medium)
+ * https://leetcode-cn.com/problems/course-schedule
+ * 深度优先搜索，时间复杂度O(m+n)，空间复杂度O(m+n)
+ *
+ * @author xing_seng
+ * @date 2020/8/4
+ */
+public class LeetCode_207_dfs {
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+}
