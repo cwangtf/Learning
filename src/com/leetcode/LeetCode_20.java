f7379a3b70e0c820230d441479bbc4f524d70a2e
xing_seng <xing_seng@hunliji.com>
2020-08-14 23:04:50 +0800

    20. 有效的括号

---
+++ src/com/leetcode/LeetCode_20.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 20. 有效的括号(easy)
+ * https://leetcode-cn.com/problems/valid-parentheses
+ * 栈，时间复杂度O(n)，空间复杂度O(n + E)
+ *
+ * @author xing_seng
+ * @date 2020/8/14
+ */
+public class LeetCode_20 {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
