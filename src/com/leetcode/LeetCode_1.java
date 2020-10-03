17f22e6dfbf35fc402b8fda7dbaf5f05937f4f1e
xing_seng <xing_seng@hunliji.com>
2020-10-03 20:51:08 +0800

    1. 两数之和

---
+++ src/com/leetcode/LeetCode_1.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和(easy)
+ * https://leetcode-cn.com/problems/two-sum/
+ * 哈希表，时间复杂度O(N)，空间复杂度O(N)
+ *
+ * @author xing_seng
+ * @date 2020/10/3
+ */
+public class LeetCode_1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
