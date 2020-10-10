572e233a029a676a04233ca1bb2f86f9b1edbf8f
xing_seng <xing_seng@hunliji.com>
2020-10-10 20:34:20 +0800

    142. 环形链表 II

---
+++ src/com/leetcode/LeetCode_142.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+import com.leetcode.utils.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 142. 环形链表 II(medium)
+ * https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ * 哈希表，时间复杂度O(n)，空间复杂度O(n)，快慢指针，时间复杂度O(n)，空间复杂度O(1)
+ *
+ * @author xing_seng
+ * @date 2020/10/10
+ */
+public class LeetCode_142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> visited = new HashSet<>();
+        while (pos != null) {
+            if (visited.contains(pos)) {
+                return pos;
+            }
+            visited.add(pos);
+            pos = pos.next;
+        }
+        return null;
+    }
+}
