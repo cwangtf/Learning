bb818dd45f6e78af0bcf12ef90dab85d9617cc5f
xing_seng <xing_seng@hunliji.com>
2020-10-12 22:48:26 +0800

    530. 二叉搜索树的最小绝对差

---
+++ src/com/leetcode/LeetCode_530.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import com.leetcode.utils.TreeNode;
+
+/**
+ * 530. 二叉搜索树的最小绝对差(easy)
+ * https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+ * 中序遍历，时间复杂度O(n)，空间复杂度O(n)
+ *
+ * @author xing_seng
+ * @date 2020/10/12
+ */
+public class LeetCode_530 {
+    int pre, ans;
+
+    public int getMinimumDifference(TreeNode root) {
+        pre = -1;
+        ans = Integer.MAX_VALUE;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre != -1) {
+            ans = Math.min(ans, (root.val - pre));
+        }
+        pre = root.val;
+        dfs(root.right);
+    }
+}
